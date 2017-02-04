cfe3c7c78040de984c74b999dd2931b3029a36e5
Dave Jacoby <jacoby.david@gmail.com>
2017-02-04 17:17:50 -0500

    changed examples

--- test.r
+++ test.r
@@ -3,7 +3,6 @@ vec  <- c( 1, 2, 3, 4, 5, 6, 5,  4,  3,  2, 1 )
 vec2 <- c( 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 )
 vec3 <- rev(vec2)
 p_cols      <- c( "blue" , "red" , "orange" , "green" ) 
-
 yrange <- range( 0 , vec , vec2 , vec3 )
 
 plot (
@@ -26,3 +25,19 @@ lines(
     )
 
 
+plot (
+    vec3 ,
+    col=p_cols[1] ,
+    type="l" ,
+    xlab="X" ,
+    ylab="Y" ,
+    ylim=yrange
+    )
+plot (
+    vec2 ,
+    col=p_cols[1] ,
+    type="b" ,
+    xlab="X" ,
+    ylab="Y" ,
+    ylim=yrange
+    )
